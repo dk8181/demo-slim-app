89b43643363a07856e0c16ddd585377b7ee9c6e4
kalnynsh <kda869@yandex.ru>
2021-12-11 18:31:40 +0300

    Added JsonResponse class.

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -4,26 +4,17 @@ declare(strict_types = 1);
 
 namespace App\Http\Action;
 
-use App\Http;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class HomeAction implements RequestHandlerInterface
 {
-    private ResponseFactoryInterface $factory;
-
-    public function __construct(ResponseFactoryInterface $factory)
-    {
-        $this->factory = $factory;
-    }
-
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $response = $this->factory->createResponse();
         $content = ['title' => 'Hello!'];
 
-        return Http::json($response, $content);
+        return new JsonResponse($content);
     }
 }
