4203c4dd77cc6cc8d16915485a265a760b8c469a
kalnynsh <kda869@yandex.ru>
2021-12-11 19:39:49 +0300

    Extracted from index to config app bootstrapping.

--- api/public/index.php
+++ api/public/index.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
-use Slim\Factory\AppFactory;
 
 http_response_code(500);
 
@@ -14,9 +13,9 @@ require __DIR__ . '/../vendor/autoload.php';
  */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
+/**
+ * @var Slim\App $app
+ */
+$app = (require __DIR__ . '/../config/app.php')($container);
 
 $app->run();
