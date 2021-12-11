8935e38144ba0a2b20f1a748123bf2cdb26ae6c2
kalnynsh <kda869@yandex.ru>
2021-12-11 18:50:07 +0300

    Extracted to config/dependencies.

--- api/public/index.php
+++ api/public/index.php
@@ -10,11 +10,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $builder = new \DI\ContainerBuilder();
 
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool) getenv('APP_DEBUG'),
-    ],
-]);
+$builder->addDefinitions(
+    require __DIR__ . '/../config/dependencies.php'
+);
 
 $container = $builder->build();
 
