7ce8e20f79bd6dd0db66c565dbf156a237ec262b
kalnynsh <kda869@yandex.ru>
2021-12-16 15:18:27 +0300

    Implemented PasswordHasher.

--- api/src/Auth/Service/PasswordHasher.php
+++ api/src/Auth/Service/PasswordHasher.php
@@ -4,8 +4,30 @@ declare(strict_types=1);
 
 namespace App\Auth\Service;
 
-interface PasswordHasher
+use Webmozart\Assert\Assert;
+
+class PasswordHasher
 {
-    public function hash(string $password): string;
-    public function validate(string $password, string $hash): bool;
+    public function hash(string $password): string
+    {
+        Assert::notEmpty($password);
+
+        /** @var string|false|null $hash */
+        $hash = \password_hash($password, PASSWORD_ARGON2ID);
+
+        if (null === $hash) {
+            throw new \RuntimeException('Invalid hashing algorithm.');
+        }
+
+        if (false === $hash) {
+            throw new \RuntimeException('Unable to generate hash.');
+        }
+
+        return $hash;
+    }
+
+    public function validate(string $password, string $hash): bool
+    {
+        return \password_verify($password, $hash);
+    }
 }
