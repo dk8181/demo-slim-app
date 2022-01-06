9cba6f3b1502a0b9d067e43c1ebebeabf8d0d3d6
kalnynsh <kda869@yandex.ru>
2022-01-06 20:53:55 +0300

    Added default error handler to ErrorMiddleware with logger.

--- api/config/common/errors.php
+++ api/config/common/errors.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use Psr\Log\LoggerInterface;
+use Slim\Middleware\ErrorMiddleware;
 use Psr\Container\ContainerInterface;
+use App\ErrorHandler\LoggedErrorHandler;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\Interfaces\CallableResolverInterface;
-use Slim\Middleware\ErrorMiddleware;
 
 return [
     ErrorMiddleware::class => static function(ContainerInterface $container): ErrorMiddleware {
@@ -19,26 +21,35 @@ return [
          * @phan-suppress MixedArrayAccess
          * @psalm-var array{
          *  display_error_details:bool,
-         *  log_errors:bool,
-         *  log_error_details:bool
          * } $config
          */
         $config = $container->get('config')['errors'];
 
-        return new ErrorMiddleware(
+        $middleware =  new ErrorMiddleware(
             $callableResolver,
             $responseFactory,
             $config['display_error_details'],
-            $config['log_errors'],
-            $config['log_error_details']
+            true,
+            true
         );
+
+        /** @var LoggerInterface logger */
+        $logger = $container->get(LoggerInterface::class);
+
+        $middleware->setDefaultErrorHandler(
+            new LoggedErrorHandler(
+                $callableResolver,
+                $responseFactory,
+                $logger
+            )
+        );
+
+        return $middleware;
     },
 
     'config' => [
         'errors' => [
             'display_error_details' => (bool) getenv('APP_DEBUG'),
-            'log_errors' => true,
-            'log_error_details' => true,
         ],
     ],
 ];
