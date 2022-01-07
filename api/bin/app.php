8a929e19ea6ebc1ad6eb9dee715a76d256d8964a
kalnynsh <kda869@yandex.ru>
2022-01-07 15:39:12 +0300

    Initialized Sentry.

--- api/bin/app.php
+++ api/bin/app.php
@@ -4,22 +4,29 @@
 declare(strict_types=1);
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Migrations\DependencyFactory;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Doctrine\Migrations\Configuration\Configuration;
-use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
-use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
-use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
-use Doctrine\Migrations\DependencyFactory;
-use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+if (\getenv('SENTRY_DSN')) {
+    \Sentry\init(['dsn' => \getenv('SENTRY_DSN')]);
+}
 
 /** @var Psr\Container\ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
 $cli = new Application('Console', '1.0.0 (stable)');
+
+if (\getenv('SENTRY_DSN')) {
+    $cli->setCatchExceptions(false);
+}
 
 /**
  * @var string[] $commands
